cbceb569991d70f411fd50b784db90a27bcac626
Reva Yoga Pradana <ryp_reaper@yahoo.com>
2016-05-08 19:07:47 +0700

    update API

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 	header( 'Access-Control-Allow-Origin: *');
-	// echo $_POST['type'] . "aaa";
-	if(!empty($_POST['type'])){
+	// echo $_POST['TYPE'] . "aaa";
+	if(!empty($_POST['TYPE'])){
 		// koneksi database
 	    $host = "localhost";        // lokasi database
 	    $user = "revoreva_goalsap";             // nama username
@@ -10,13 +10,13 @@
 	    $cn = mysqli_connect( $host, $user, $pass, $dbname );
 	    
 	    //url/login/email/password
-	    if ( $_POST['type'] == "login" ) {
+	    if ( $_POST['TYPE'] == "login" ) {
 	    	$query = 	"SELECT
 	    					AKUN_ID, EMAIL, USERNAME, FOTO
 		                from
 		                 	AKUN
 		                where
-		                  AKUN.EMAIL = \"" . $_POST['p1'] . "\" AND AKUN.PASSWORD = \"" . $_POST['p2'] . "\"
+		                  AKUN.EMAIL = \"" . $_POST['EMAIL'] . "\" AND AKUN.PASSWORD = \"" . $_POST['PASSWORD'] . "\"
 		                limit 1";
 	    	
 		    $result = mysqli_query( $cn, $query );
@@ -30,11 +30,10 @@
 		    	echo '{"status":200, "message":"login sukses", "data":' . json_encode( $var ) . '}';
 		    else
 		    	echo '{"status":200, "message":"login gagal"}';
-	    } else if($_POST['type'] == 'register'){
+	    } else if($_POST['TYPE'] == 'register'){
 	    	//register/username/email/password/foto
-	    	//register/p1      /p2   /p3      /p4
 
-	    	$query = "INSERT INTO AKUN(USERNAME, EMAIL, PASSWORD, FOTO) VALUES (" . $_POST['p1'] . "," . $_POST['p2'] . "," . $_POST['p3'] . "," . $_POST['p4'] . ")";
+	    	$query = "INSERT INTO AKUN(USERNAME, EMAIL, PASSWORD, FOTO) VALUES (" . $_POST['USERNAME'] . "," . $_POST['EMAIL'] . "," . $_POST['PASSWORD'] . "," . $_POST['FOTO'] . ")";
         	
 		    $result = mysqli_query( $cn, $query );
 		    
@@ -44,7 +43,7 @@
 		                from
 		                 	AKUN
 		                where
-		                  AKUN.EMAIL = \"" . $_POST['p2'] . "\"
+		                  AKUN.EMAIL = \"" . $_POST['EMAIL'] . "\"
 		                limit 1";
 	    	
 			    $result = mysqli_query( $cn, $query );
@@ -57,6 +56,36 @@
 		    }
 		    else
 		    	echo '{"status":200, "message":"registrasi gagal"}';
+	    }
+	} else if(!empty($_GET['TYPE'])){
+		// koneksi database
+	    $host = "localhost";        // lokasi database
+	    $user = "revoreva_goalsap";             // nama username
+	    $pass = "g04l54pp";                 // password database
+	    $dbname = "revoreva_goalsapp";       // nama database yang digunakan
+	    $cn = mysqli_connect( $host, $user, $pass, $dbname );
+
+	    if($_GET['TYPE'] == 'list_target'){
+	    	//list_target/AKUN_ID
+	    	
+	    	$query = 	"SELECT
+	    					TARGET_ID, FOTO, SALDO, HARGA, DUE_DATE
+		                FROM
+		                 	TARGET
+		                WHERE
+		                  	TARGET.AKUN_ID = " . $_POST['AKUN_ID'];
+	    	
+		    $result = mysqli_query( $cn, $query );
+		    $var = array();
+		    $ada = 0;
+		    while ( $obj = mysqli_fetch_object( $result ) ) {
+		    	$var[] = $obj;
+		    	$ada = 1;
+		    }
+		    if($ada == 1)
+		    	echo '{"status":200, "message":"get list target sukses", "data":' . json_encode( $var ) . '}';
+		    else
+		    	echo '{"status":200, "message":"get list target gagal"}';
 	    }
 	}
 ?>
